5be781826f8210ae7e22f56447285fda354c9e11
n-are-n <naren000000000@gmail.com>
2025-09-05 13:59:24 +0530

    add code coverage & CI/CD Pipeline configuration

--- vite.config.ts
+++ vite.config.ts
@@ -15,5 +15,24 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    coverage: {
+      reporter: ['text', 'json', 'html', 'xml'],
+      provider: 'v8',
+      exclude: [
+        'node_modules/',
+      //   'src/main.ts',
+      //   'src/router/**',
+      //   'src/**/*.d.ts',
+      //   'src/**/*.spec.ts',
+      //   'vite.config.*',
+      //   'vitest.config.*',
+      //   'cypress.config.*'
+      // ],
+      // include: ['src/**/*.{js,ts,vue}'],
+    }
   }
 })
